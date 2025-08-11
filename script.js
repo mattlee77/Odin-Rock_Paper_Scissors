a20b2d66dd77610a64901be9e60f0a3218f63974
mattlee77 <mattwylee@gmail.com>
2025-08-11 16:39:21 +0800

    Created function to play the game 5 times and keep score using a for loop

--- script.js
+++ script.js
@@ -10,5 +10,42 @@ function getHumanChoice() {
     return choice;
 }
 
-getHumanChoice();
-console.log(getComputerChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        function playRound(humanSelection, computerSelection) {
+            if (humanSelection === computerSelection) {
+                console.log("It's a tie!");
+            } else if (
+                (humanSelection === 'rock' && computerSelection === 'scissors') ||
+                (humanSelection === 'paper' && computerSelection === 'rock') ||
+                (humanSelection === 'scissors' && computerSelection === 'paper')
+            ) {
+                humanScore++;
+                console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
+            } else {
+                computerScore++;
+                console.log(`You lose! ${computerSelection} beats ${humanSelection}.`);
+            }
+        };
+        playRound(humanSelection, computerSelection);
+        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+    }
+
+    console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+    } else if (humanScore < computerScore) {
+        console.log("You lose the game!");
+    } else {
+        console.log("The game is a tie!");
+    }
+    return { humanScore, computerScore };
+}
+
+
+playGame();
